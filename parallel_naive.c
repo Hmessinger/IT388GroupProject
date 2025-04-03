ca643154d33e9087e7024f03207a456641f5f88e
Hogan Messinger <messinger8771@gmail.com>
2025-04-03 16:37:57 -0500

    Deleted some files and added initial description to sequential_naive

--- parallel_naive.c
+++ parallel_naive.c
@@ -1,11 +1,11 @@
-/** FILE: parallelTRW.c
+/** FILE: parallel_naive.c
  *  Description: Here we are adding openMP to the sequential (naive) version of the trapping rainwater problem. This will parallelize the function that
  *               calculates the amount of rainwater that can be trapped. This approach will speedup the time complexity because there will be multiple
  *               threads working on the computations.
  *  Compile:
- *          gcc -o parNaive parallelNaiveTRW.c
+ *          gcc -o par_naive parallel_naive.c
  *  Execution:
- *          ./parNaive
+ *          ./par_naive <array size>
  *
  * Name: Hogan Messinger, Suhail Tailor, Derek Kmieciak, Angel Hernandez
  * Course: IT 388-Spring 2025
--- sequential_naive.c
+++ sequential_naive.c
@@ -1,3 +1,17 @@
+/** FILE: sequential_naive.c
+ *  Description: Here we are implementing the sequential version of the trapping rainwater problem. This version iterates
+ *               through the array of values and it finds the highest bars on teh left and right sides. Then it will take the smaller of the two heights
+ *               to find how much rainwater can be trapped in between those two bars. This approach takes a time of O(n^2).
+ *  Compile:
+ *          gcc -o seq_naive sequential_naive.c
+ *  Execution:
+ *          ./seq_naive <array size>
+ *
+ * Name: Hogan Messinger, Suhail Tailor, Derek Kmieciak, Angel Hernandez
+ * Course: IT 388-Spring 2025
+ * Group Project
+ */
+
 /*
 Sequential approach to the trapping rainwater problem.
 Naive approach: O(n^2) double loop
@@ -5,7 +19,7 @@ Naive approach: O(n^2) double loop
 Code credit: Geeks For Geeks
     https://geeksforgeeks.org/trapping-rain-water/#
 
-This code will be modified into the parallel final code. 
+This code will be modified into the parallel final code.
 
 Compile: gcc -o seq_naive sequential_naive.c
 Execute: ./seq_naive <array size>
@@ -17,27 +31,27 @@ Execute: ./seq_naive <array size>
 int maxWater(int arr[], int n)
 {
     int result = 0;
-    //For every element of the array
-    for(int i=1; i<n-1; i++)
+    // For every element of the array
+    for (int i = 1; i < n - 1; i++)
     {
-        //Find max to the left
+        // Find max to the left
         int left = arr[i];
-        for(int j=0; j<i; j++)
+        for (int j = 0; j < i; j++)
         {
-            if(arr[j]>left)
+            if (arr[j] > left)
                 left = arr[j];
         }
-        //Find max to the right
-        int right = 0; 
-        for(int j=i+1; j<n; j++)
+        // Find max to the right
+        int right = arr[i];
+        for (int j = i + 1; j < n; j++)
         {
-            if(arr[j]>right)
+            if (arr[j] > right)
                 right = arr[j];
         }
-        //Update max water result
-        result += (left < right ? left : right ) - arr[i];
+        // Update max water result
+        result += (left < right ? left : right) - arr[i];
     }
-    
+
     return result;
 }
 
@@ -46,26 +60,25 @@ int main(int argc, char *argv[])
     int n;
     int *arr;
 
-    if(argc !=2 )
+    if (argc != 2)
     {
         fprintf(stderr, "\nIncorrect number of arguments\n\t---USAGE: ./seq_naive <array size>\n\n");
         exit(1);
     }
-    n = atoi(argv[1]); //Get array size
-    arr = (int*)malloc(n*sizeof(int));
-    //Generate random array of n integers
-    //Array "heights" will generate between 0 and 15
-    for(int i=0; i<n; i++)
+    n = atoi(argv[1]); // Get array size
+    arr = (int *)malloc(n * sizeof(int));
+    // Generate random array of n integers
+    // Array "heights" will generate between 0 and 15
+    for (int i = 0; i < n; i++)
     {
-        //TODO: "Random" array is generated the name for same n every time
+        // TODO: "Random" array is generated the name for same n every time
         arr[i] = (int)((double)rand() / ((double)RAND_MAX + 1) * 16);
     }
-    
-    
+
     printf("Array:\n");
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("[%d] ",arr[i]);
+        printf("[%d] ", arr[i]);
     }
     printf("\n---------------------------------------\n");
     printf("Maximum trapped rainwater: %d units\n", maxWater(arr, n));
