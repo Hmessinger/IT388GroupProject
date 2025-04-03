95eaebe73427e0bc8b63292459796da37f9692e6
DEREK <dgkmiecisu@ilstu.edu>
2025-04-03 15:46:28 -0500

    TEST - Sequential Naive approach file upload

---
+++ sequential_naive.c
@@ -0,0 +1,22 @@
+/*
+Sequential approach to the trapping rainwater problem.
+Naive approach: O(n^2) double loop
+
+Code credit: Geeks For Geeks
+    https://geeksforgeeks.org/trapping-rain-water/#
+
+This code will be modified into the parallel final code. 
+
+Compile: gcc -o seq_naive sequential_naive.c
+Execute: ./seq_naive <array size>
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    
+    return 0;
+}
