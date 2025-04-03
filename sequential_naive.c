06befcc6fe6300e8f982c1f15b6220a3e0a5cbe3
DEREK <dgkmiecisu@ilstu.edu>
2025-04-03 16:08:25 -0500

    Array size input and random array generation

--- sequential_naive.c
+++ sequential_naive.c
@@ -16,7 +16,32 @@ Execute: ./seq_naive <array size>
 
 int main(int argc, char *argv[])
 {
+    int n;
+    int *arr;
 
+    if(argc !=2 )
+    {
+        fprintf(stderr, "\nIncorrect number of arguments\n\t---USAGE: ./seq_naive <array size>\n\n");
+        exit(1);
+    }
+    n = atoi(argv[1]); //Get array size
+    arr = (int*)malloc(n*sizeof(int));
+    //Generate random array of n integers
+    //Array "heights" will generate between 0 and 15
+    for(int i=0; i<n; i++)
+    {
+        //TODO: "Random" array is generated the name for same n every time
+        arr[i] = (int)((double)rand() / ((double)RAND_MAX + 1) * 16);
+    }
     
+    //Working code here...
+    printf("TEST:\n");
+    for(int i=0; i<n; i++)
+    {
+        printf("[%d] ",arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
     return 0;
 }
